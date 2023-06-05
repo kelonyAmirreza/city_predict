0d575d0d5e386d59b2a7fdc0110e8ef0659195cf
Amirreza <ghodrati.amirreza@gmail.com>
2023-06-05 11:49:15 +0200

    database updated

---
+++ README.md
@@ -0,0 +1,17 @@
+# Data Processing Utility
+
+This utility provides functions for data processing and manipulation. It includes the following functionalities:
+
+-   Fetching all available dataflows from ISTAT.
+-   Saving data to JSON files.
+-   Finding key-value pairs in an object based on a provided word.
+
+## Run
+
+To run the utility, use the following command:
+
+```
+yarn start [keyword]
+```
+
+Replace `[keyword]` with the desired word for which you want to fetch data. Only one keyword is acceptable. If no keyword is provided, the `Import price` will use a default keyword.
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { all_available } from "./discovery"
 import { saveToJson, findKeyValuePairsWithWord } from "./utils"
 import { ISTAT } from "./base"
-
 import all_available_requests from "../assets/all_available_requests.json"
+import { log } from "console"
 
 /**
  * Main function to fetch data and process it.
@@ -19,26 +19,63 @@ async function main() {
     }
 
     // Find key-value pairs with the word "Import price" in the all_available_requests object
-    const request_object = findKeyValuePairsWithWord(
-        "Import price",
-        all_available_requests
-    )
+    const args = process.argv.slice(2)
+    if (args[0] === "all") {
+        const request_object = await getAllData()
 
-    // Save the request_object to a JSON file
-    saveToJson("request_object", JSON.stringify(request_object))
+        // Save the request_object to a JSON file
+        saveToJson("request_object", JSON.stringify(request_object))
 
-    const client = new ISTAT()
-    let result: any = {}
-    for (const key in request_object) {
-        // Get data using the request object and a start date of "2000-01-01"
-        result[key] = (
-            await client.get_data(request_object[key], "2000-01-01")
-        ).data
-        break
+        const client = new ISTAT()
+        let result: any = {}
+        for (const key in request_object) {
+            result = {}
+            // Get data using the request object and a start date of "2000-01-01"
+            result[key] = (
+                await client.get_data(request_object[key], "2000-01-01")
+            ).data
+
+            // Save the result to a JSON file
+            saveToJson(request_object[key].id, JSON.stringify(result))
+        }
+    } else {
+        const keyword = args.length === 0 ? "Import price" : args[0]
+        const request_object = findKeyValuePairsWithWord(
+            keyword,
+            all_available_requests
+        )
+
+        // Save the request_object to a JSON file
+        saveToJson("request_object", JSON.stringify(request_object))
+
+        const client = new ISTAT()
+        let result: any = {}
+        for (const key in request_object) {
+            // Get data using the request object and a start date of "2000-01-01"
+            result[key] = (
+                await client.get_data(request_object[key], "2020-01-01")
+            ).data
+        }
+
+        // Save the result to a JSON file
+        saveToJson("result", JSON.stringify(result))
+    }
+}
+
+async function getAllData(
+    startPeriod?: string,
+    obj: any = all_available_requests
+) {
+    startPeriod = startPeriod ? startPeriod : "2020-01-01"
+
+    // Object to store the key-value pairs
+    const keyValuePairs: { [key: string]: any } = {}
+
+    for (const key in obj.references) {
+        keyValuePairs[obj.references[key].name] = obj.references[key]
     }
 
-    // Save the result to a JSON file
-    saveToJson("result", JSON.stringify(result))
+    return keyValuePairs
 }
 
 // Call the main function
